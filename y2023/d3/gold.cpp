b3f3f28b67428333bd32be91abd8676c2e908e79
martianbit <martianbit@proton.me>
2023-12-03 11:27:39 +0100

    do day 3

---
+++ y2023/d3/gold.cpp
@@ -0,0 +1,48 @@
+#include <aoc.hpp>
+
+vector<string> lines;
+
+void parse(string s, ll li) {
+    lines.push_back(s + '.');
+}
+
+void master() {
+    ll nx, ny;
+    string cn;
+    unordered_set<ll> gears;
+    unordered_map<ll, vector<ll>> adj;
+
+    loop(lines, i, x) {
+        loop(x, j, y) {
+            if(!isdigit(y)) {
+                if(!cn.empty()) {
+                    for(const auto &z : gears) {
+                        if(adj.find(z) == adj.end())
+                            adj[z] = {};
+                        adj[z].push_back(stoi(cn));
+                    }
+
+                    gears.clear();
+                }
+
+                cn = "";
+                continue;
+            }
+
+            cn += y;
+
+            for(const auto &z : D8) {
+                nx = j + z[0];
+                ny = i + z[1];
+
+                if(nx >= 0 && nx < x.size() && ny >= 0 && ny < lines.size() && lines[ny][nx] == '*')
+                    gears.insert(ny * x.size() + nx);
+            }
+        }
+    }
+
+    ans = accumulate(all(adj), 0, [](auto a, auto b) {
+        return a + ((b.second.size() == 2) ? reduce(all(b.second), 1, multiplies()) : 0);
+    });
+}
+
---
+++ y2023/d3/silver.cpp
@@ -0,0 +1,43 @@
+#include <aoc.hpp>
+
+vector<string> lines;
+
+void parse(string s, ll li) {
+    lines.push_back(s + '.');
+}
+
+void master() {
+    bool ok;
+    ll nx, ny;
+    string cn;
+
+    ok = false;
+
+    loop(lines, i, x) {
+        loop(x, j, y) {
+            if(!isdigit(y)) {
+                if(!cn.empty() && ok)
+                    ans += stoi(cn);
+
+                ok = false;
+                cn = "";
+
+                continue;
+            }
+
+            cn += y;
+
+            for(ll k = 0; !ok && k < 8; k++) {
+                nx = j + D8[k][0];
+                ny = i + D8[k][1];
+
+                ok = (nx >= 0 &&
+                      nx < lines[0].size() &&
+                      ny >= 0 &&
+                      ny < lines.size() &&
+                      lines[ny][nx] != '.' && !isdigit(lines[ny][nx]));
+            }
+        }
+    }
+}
+
