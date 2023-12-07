26335b6b186bba84d4b494f521e06be440ddc3c5
martianbit <martianbit@proton.me>
2023-12-07 09:25:33 +0100

    solve day 7

---
+++ y2023/d7/silver.cpp
@@ -0,0 +1,64 @@
+#include <aoc.hpp>
+
+unordered_map<string, ll> bids;
+
+void parse(string s, stringstream &ss, ll li) {
+    auto p1 = split(s);
+    bids[p1[0]] = stoll(p1[1]);
+}
+
+void master() {
+    ll type, rank;
+    array<vector<string>, 7> types;
+
+    for(const auto &x : bids) {
+        unordered_map<char, ll> counts;
+
+        for(const auto &y : x.first) {
+            if(counts.find(y) == counts.end())
+                counts[y] = 0;
+
+            counts[y]++;
+        }
+
+        vector<ll> N;
+        transform(all(counts), back_inserter(N), [](auto a) { return a.second; });
+        sort(all(N), greater());
+
+        if(N.size() == 1)
+            type = 6;
+        else if(N.size() == 2 && N[0] == 4)
+            type = 5;
+        else if(N.size() == 2 && N[0] == 3)
+            type = 4;
+        else if(N.size() == 3 && N[0] == 3)
+            type = 3;
+        else if(N.size() == 3 && N[0] == 2)
+            type = 2;
+        else if(N.size() == 4)
+            type = 1;
+        else
+            type = 0;
+
+        types[type].push_back(x.first);
+    }
+
+    rank = 1;
+
+    for(auto &x : types) {
+        sort(all(x), [](auto a, auto b) {
+            string strength = "AKQJT98765432";
+
+            for(ll i = 0; i < 5; i++) {
+                if(a[i] != b[i])
+                    return strength.find(a[i]) > strength.find(b[i]);
+            }
+
+            return false;
+        });
+
+        for(const auto &y : x)
+            ans += rank++ * bids[y];
+    }
+}
+
